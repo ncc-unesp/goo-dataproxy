a77c9891859879319dab06960523e0c5d6048c05
Beraldo Leal <beraldo@beraldoleal.com>
2013-02-20 16:03:48 -0300

    local backup creates dir if does not exists

--- core/storage/backends/local.py
+++ core/storage/backends/local.py
@@ -9,6 +9,10 @@ class LocalStorage(GenericStorage):
     def upload(self, file, name):
         dst = urlparse(settings.STORAGE_BASE_URI).path
         output = "%s/%s" % (dst, name)
+
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+
         with open(output, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
@@ -17,7 +21,7 @@ class LocalStorage(GenericStorage):
         filename = url.replace("local://", "")
         wrapper = FileWrapper(file(filename))
         response = HttpResponse(wrapper, content_type='aplication/octet-stream')
-        response['Content-Disposition'] = 'filename="somefilename.pdf"'
+        response['Content-Disposition'] = 'filename="somefilename.zip"'
         response['Content-Length'] = os.path.getsize(filename)
         return response
 
