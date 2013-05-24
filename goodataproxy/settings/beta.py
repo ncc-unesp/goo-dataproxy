8caf2903939e3766099364793ed6b05d9be9613e
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2013-05-24 17:05:44 -0300

    Change beta deploy to use GSIFTP protocol

--- goodataproxy/settings/beta.py
+++ goodataproxy/settings/beta.py
@@ -4,6 +4,6 @@ from defaults import *
 DEBUG = True
 
 # Local or GridFTP
-STORAGE_BASE_URI = "local:///tmp/"
+STORAGE_BASE_URI = "gsiftp://submit.grid.unesp.br/store/gridunesp/goo-beta"
 
 GOO_SERVER_URI = "https://beta.grid.unesp.br/api/v1/"
